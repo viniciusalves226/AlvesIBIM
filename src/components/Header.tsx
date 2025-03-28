879715396cec425d7e4fe9b82b42db3cb4a961cd
vinicius <tallytha.preciliano@eaportal.org>
2025-03-28 11:32:37 -0300

    readme

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+function Header(){
+return (
+    <>
+        <header>
+        <div className="Box">
+          <div className="GameMania">
+            <div className="logo"></div>
+          </div>
+        </div>
+      </header>
+    </>
+
+ )
+ 
+}
